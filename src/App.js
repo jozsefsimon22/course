7652b7ce32cfc02a54eeb4957343deeef48f5d61
jozsefsimon22 <nex.simon@gmail.com>
2025-06-08 01:20:26 +0200

    DemoReactApp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import GoalList from "./components/GoalList/GoalList";
+import NewGoal from "./components/NewGoal/NewGoal";
 
 function App() {
+  const [courseGoals, setCourseGoals] = useState([
+    { id: "cg1", text: "Finish the course" },
+    { id: "cg2", text: "Learn all about the Course Main Topic" },
+    { id: "cg3", text: "Help other students in the Course Q&amp;A" },
+  ]);
+
+  function addNewGoalHandler(newGoal) {
+    // setCourseGoals(courseGoals.concat(newGoal))
+    setCourseGoals((prevCourseGoals) => prevCourseGoals.concat(newGoal));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="course-goals">
+      <h2>Course Goals</h2>
+      <NewGoal onAddGoal={addNewGoalHandler} />
+      <GoalList goals={courseGoals} />
     </div>
   );
 }
---
+++ src/components/NewGoal/NewGoal.js
@@ -0,0 +1,33 @@
+import './NewGoal.css'
+import { useState } from 'react';
+
+function NewGoal(props){
+
+    const [enteredText, setEnteredText] = useState('');
+
+    function addGoalHandler(event){
+        event.preventDefault();
+
+        const newGoal = {
+            id: Math.random().toString(),
+            text: enteredText
+        }
+
+        props.onAddGoal(newGoal)
+        setEnteredText('')
+
+    }
+
+    function textChangeHandler(event){
+        setEnteredText(event.target.value)
+    }
+
+    return(
+        <form className='new-goal' onSubmit={addGoalHandler}>
+            <input type="text" value={enteredText} onChange={textChangeHandler}/>
+            <button type="submit">Add Goal</button>
+        </form>
+    )
+}
+
+export default NewGoal;
